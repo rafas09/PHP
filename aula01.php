62bfb94a7b7f60c3d41f23742b1bc11d2dfe0d49
Rafaella Ferreira Monteiro <rafaellamonteiro555@gmail.com>
2025-05-08 12:09:32 -0300

    Add files via upload

---
+++ aula01.php
@@ -0,0 +1,9 @@
+<h1>Minha primeira página PHP<h1>
+    <?php
+    echo "Olá, Mundo!"; // Exibe a mensagem "Olá, Mundo!" na tela
+    echo "<br>"; // Quebra de linha
+    echo "<br>";
+    echo "Meu nome é Rafaella";
+    echo "<br>"; // Quebra de linha
+    echo "<p>Estou aprendendo PHP! </p>"; 
+    echo "<p>Estou gostando PHP! </p>"; 
